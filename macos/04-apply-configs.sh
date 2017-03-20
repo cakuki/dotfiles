e20bbcb0d378b67a9d2c8d7ded2e09c51144f3ba
Can Kutlu Kinay <ckkinay@gmail.com>
2017-03-20 10:09:01 +0100

    Apply npm & vscode configurations

--- macos/04-apply-configs.sh
+++ macos/04-apply-configs.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+# npm configuration
+ npm config set init.author.name "Can Kutlu Kinay"
+ npm config set init.author.email "me@ckk.im"
+ npm config set init.author.url "http://ckk.im"
+
+ # vscode
+ cp ../conf/vscode/user.json $HOME/Library/Application\ Support/Code/User/settings.json
