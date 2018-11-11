27e57e11190c261036c3a4e18442c14688ac26b9
Can Kutlu Kınay <ckkinay@gmail.com>
2018-11-11 19:26:19 +0100

    Update 02-brew-formulas.sh

--- macos/02-brew-formulas.sh
+++ macos/02-brew-formulas.sh
@@ -1,32 +1,59 @@
 #!/bin/bash
 
-# Install formulas
-brew install \
-  axel \
-  docker-cloud \
-  ghi \
-  git \
-  git-extras \
-  hub \
-  imgcat \
-  jq \
-  nvm \
-  python \
-  python3 \
-  ruby \
-  unrar \
-  yarn
+FORMULAS=(
+  ### Development ###
+    go
+    node
+    nvm
+    python
+    python3
+    ruby
+    yarn
+  ### File ###
+    the_silver_searcher # Search tool 'ag'
+    tree
+    unrar
+  ### Network ###
+    axel          # accelerated download
+    hostess       # manipulate /etc/hosts
+  ### Orchestration ###
+    docker-cloud
+    kube-ps1      # k8s ns/ctx in prompt
+    kubectx       # k8s context switcher
+    stern         # k8s logs
+  ### Version Tracking ###
+    ghi           # github issues
+    git
+    git-lfs
+    git-extras
+    hub           # github integration, git wrapper
+    tig           # Better 'git log'
+    zaquestion/tap/lab # gitlab integration, git wrapper
+  ### Other ###
+    asciinema # terminal recorder
+    jq        # JSON processor
+    bash-completion
+    zsh-completions
+  )
 
-# Install cask formulas
+# Install brew formulas
+brew install "${FORMULAS[@]}"
+
+
+# Install casks
 brew cask install \
+  authy \
   appcleaner \
   docker \
+  google-chrome \
+  google-cloud-sdk \
   imageoptim \
   iterm2 \
   java \
-  mongohub \
+  keybase \
   skype \
-  visual-studio-code
+  visual-studio-code \
+  vlc
 
 # Set nvm home
 mkdir -p ~/Library/Developer/nvm
@@ -37,5 +64,5 @@ export NVM_DIR="$HOME/Library/Developer/nvm"
 . "/usr/local/opt/nvm/nvm.sh"
 EOF
 
-# Required by hub
-/usr/local/bin/gem install pygments.rb
+# Required by hub (not sure now, will check again)
+# /usr/local/bin/gem install pygments.rb
